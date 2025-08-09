2e79b72b05b57e74b3ad9df713cbb754b25b79c4
ronit0000 <ronitkusahu@gmail.com>
2025-08-09 12:36:12 +0530

    m

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -23,7 +23,7 @@
  */
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/DashBoard";
