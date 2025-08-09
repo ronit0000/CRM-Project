3cd8c8f250cc89e9d43b6311c1474dd8752b8bcb
ronit0000 <ronitkusahu@gmail.com>
2025-08-09 11:23:34 +0530

    Added some changes

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// vite.config.js
 export default defineConfig({
-  base: "/CRM-Project/",
+  base: process.env.NODE_ENV === 'production' ? '/CRM-Project/' : '/',
   plugins: [react()],
-});
+})
